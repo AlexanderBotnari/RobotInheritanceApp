f87176c565f260b72efde72c6e6b2b7f0cda5511
AlexanderBotnari <63365662+AlexanderBotnari@users.noreply.github.com>
2020-05-13 02:10:28 -0700

    Update Application.java

--- src/main/Application.java
+++ src/main/Application.java
@@ -10,10 +10,10 @@ public class Application {
         public static void main(String[] args) {
 		
      	AbstractRobot robo = RobotFactory.getRobot("Alpha","Bob");
-		AbstractRobot robo1 = RobotFactory.getRobot("Beta","Jack");
-		AbstractRobot robo2 = RobotFactory.getRobot("Charlie","Optimus");
+	AbstractRobot robo1 = RobotFactory.getRobot("Beta","Jack");
+	AbstractRobot robo2 = RobotFactory.getRobot("Charlie","Optimus");
 		
-		System.out.printf("Simple test passed %d%%\n", SimpleTest.runCase(robo));
+	System.out.printf("Simple test passed %d%%\n", SimpleTest.runCase(robo));
         System.out.printf("Simple test passed %d%%\n", ChargeTest.chargeTest(robo1));
         System.out.printf("Simple test passed %d%%\n", DiagonalWalkTest.diagonalTest(robo2));
 		
