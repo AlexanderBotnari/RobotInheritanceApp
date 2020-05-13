2c0d2d26827a17ec4f938680147e174bd40679ec
AlexanderBotnari <63365662+AlexanderBotnari@users.noreply.github.com>
2020-05-13 02:06:21 -0700

    Update RobotFactory.java

--- src/robo/RobotFactory.java
+++ src/robo/RobotFactory.java
@@ -4,8 +4,8 @@ public class RobotFactory {
 	
 	public static AbstractRobot getRobot(String model, String name) {
 		       
-		if (model.equals("Alpha")) 
-			 return new AlphaRobot(name, model);
+	    if (model.equals("Alpha")) 
+	            return new AlphaRobot(name, model);
 	            
 	    if (model.equals("Beta")) 
 	            return new BetaRobot(name, model);
